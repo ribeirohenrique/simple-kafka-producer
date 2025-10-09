891cf5ccc0bb2e39a4b2c80218f6c9624e98be6b
Henrique Mendes <henrique1258@gmail.com>
2025-10-09 18:37:25 -0300

    simplificando projeto, reduzindo dependencias

--- src/main/java/com/kafka/producer/Application.java
+++ src/main/java/com/kafka/producer/Application.java
@@ -1,6 +1,5 @@
 package com.kafka.producer;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        Dotenv.configure().load().entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        //Dotenv.configure().load().entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(Application.class, args);
     }
 
